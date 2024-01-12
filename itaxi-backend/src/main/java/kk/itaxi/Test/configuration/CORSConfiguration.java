1d40e6857fe2decfbd57fa98226d388cb011f943
kamil.kurzyna <kamilkurzyna@gmail.com>
2024-01-12 09:27:58 +0100

    cors fix

--- itaxi-backend/src/main/java/kk/itaxi/Test/configuration/CORSConfiguration.java
+++ itaxi-backend/src/main/java/kk/itaxi/Test/configuration/CORSConfiguration.java
@@ -11,7 +11,7 @@ public class CORSConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
+        registry.addMapping("/**").allowedOrigins("http://localhost:3000").allowedMethods("*").allowedHeaders("*");
     }
 
 }
